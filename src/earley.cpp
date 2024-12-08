94023fb1096bae468b85de50366619b50ba64066
Mishenkov Daniil <mishenkovdaniil@mail.ru>
2024-12-08 13:25:28 +0300

    implement earley algo

--- src/earley.cpp
+++ src/earley.cpp
@@ -13,26 +13,45 @@ bool Earley::predict (const std::string& string) {
 }
 
 bool Earley::predict (const char *string) {
+    // grammar_.Print();
     size_t len = strlen(string);
-
+    // printf ("1\n");
     std::vector<std::set<Situation>> situation_lists(len + 1);
+    // printf ("2\n");
     situation_lists[0].insert(Situation(grammar_.rules__[grammar_.start_][0], 0, 0));
+    // printf ("3\n");
 
     for (size_t j = 0; j < len + 1; ++j) {
+        // printf ("4\n");
         scan (situation_lists, j, string, len);
         size_t last = -1;
+        // printf ("5\n");
         while (situation_lists[j].size() != last) {
+            // printf ("6\n");
             last = situation_lists[j].size();
+            // printf ("7\n");
             complete (situation_lists, j, string, len);
+            // printf ("8\n");
             predict (situation_lists, j, string, len);
+            // printf ("9\n");
         }
+        // printf ("10\n");
+        // for (size_t i = 0; i <= j; ++i) {
+        //     std::cout << "situations " << i << "\n";
+        //     for (auto sit:situation_lists[i]) {
+        //         std::cout << "\t";
+        //         sit.Print();
+        //     }
+        //     std::cout << "\n";
+        // }
     }
+    // printf ("11\n");
 
-    return situation_lists[len].count(Situation(grammar_.rules__[grammar_.start_][0], 1, len));
+    return situation_lists[len].count(Situation(grammar_.rules__[grammar_.start_][0], 1, 0));
 }
 
 
-void Earley::scan(std::vector<std::set<Situation>> situation_lists, size_t cur_sit_idx, const char *string, size_t pos) {
+void Earley::scan(std::vector<std::set<Situation>>& situation_lists, size_t cur_sit_idx, const char *string, size_t pos) {
     if (cur_sit_idx == 0)
         return;
     for (auto situation: situation_lists[cur_sit_idx - 1]) {
@@ -43,23 +62,36 @@ void Earley::scan(std::vector<std::set<Situation>> situation_lists, size_t cur_s
     }
 } 
 
-void Earley::predict(std::vector<std::set<Situation>> situation_lists, size_t cur_sit_idx, const char *string, size_t pos) {
+void Earley::predict(std::vector<std::set<Situation>>& situation_lists, size_t cur_sit_idx, const char *string, size_t pos) {
     for (auto situation: situation_lists[cur_sit_idx]) {
         GrammarSymbol symb = situation.rule_.right_[situation.pos_in_rule_];
-        if (symb.isTerminal_)
+        if (symb.isTerminal_) {
+            // printf ("terminal\n");
             continue;
-        NeTerminal neTerminal = *(reinterpret_cast<NeTerminal *>(&situation.rule_.right_[situation.pos_in_rule_]));
-        for (auto rule:grammar_.rules__[neTerminal]) {
+        }
+        NeTerminal neTerminal = *(reinterpret_cast<NeTerminal *>(&(situation.rule_.right_[situation.pos_in_rule_])));
+        for (auto rule: grammar_.rules__[neTerminal]) {
+            // printf ("insert");
+            // std::cout << neTerminal.GetAsString() << "\n";
+            // rule.Print();
+            // printf ("%d\n", situation_lists[cur_sit_idx].count(Situation(rule, 0, cur_sit_idx)));
             situation_lists[cur_sit_idx].insert(Situation(rule, 0, cur_sit_idx));
         }
     }
 } 
 
-void Earley::complete(std::vector<std::set<Situation>> situation_lists, size_t cur_sit_idx, const char *string, size_t pos) {
-    for (auto sit: situation_lists[cur_sit_idx - 1]) {
+void Earley::complete(std::vector<std::set<Situation>>& situation_lists, size_t cur_sit_idx, const char *string, size_t pos) {
+    // printf("$1\n");
+    for (auto sit: situation_lists[cur_sit_idx]) {
+        // printf("$2\n");
+        if((sit.rule_.right_.size() != sit.pos_in_rule_) &&
+            !(sit.rule_.right_[0] == Epsilon))
+            continue;
         size_t i = sit.pos_in_word_;
         for (auto situation: situation_lists[i]) {
-            if (situation.pos_in_word_ == cur_sit_idx) {
+            // printf("$3, %d\n", i);
+            if (!(situation.rule_.right_[situation.pos_in_rule_].isTerminal_)) {
+                // printf("$5\n");
                 situation_lists[cur_sit_idx].insert(Situation(situation.rule_, situation.pos_in_rule_ + 1, situation.pos_in_word_));
             }
         }
--- src/earley.hpp
+++ src/earley.hpp
@@ -12,7 +12,44 @@ public:
         rule_(rule), pos_in_rule_(pos_in_rule), pos_in_word_(pos_in_word) {}
 
     bool operator < (const Situation& other)const {
-        return pos_in_rule_ < other.pos_in_rule_ && pos_in_word_ < other.pos_in_word_;
+        if (pos_in_rule_ < other.pos_in_rule_)
+            return true;
+        if (pos_in_rule_ == other.pos_in_rule_) {
+            if (pos_in_word_ < other.pos_in_word_) {
+                return true;
+            }
+            if (pos_in_word_ == other.pos_in_word_) {
+                if (rule_.left_ < other.rule_.left_) {
+                    return true;
+                }
+                if (rule_.left_ == other.rule_.left_) {
+                    if (rule_.right_.size() < other.rule_.right_.size()) {
+                        return true;
+                    }
+                    if (rule_.right_.size() == other.rule_.right_.size()) {
+                        size_t i = 0;
+                        while (i < rule_.right_.size() && rule_.right_[i] == other.rule_.right_[i]) {
+                            ++i;
+                        }
+                        if (i < rule_.right_.size() && rule_.right_[i] < other.rule_.right_[i]) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    void Print () {
+        std::cout << "Situation:" << rule_.left_.GetAsString() << "->";
+        for (size_t i = 0; i < pos_in_rule_; ++i) {
+            std::cout << rule_.right_[i].GetAsString();
+        }
+        std::cout << "*";
+        for (size_t i = pos_in_rule_, max = rule_.right_.size(); i < max; ++i) {
+            std::cout << rule_.right_[i].GetAsString();
+        }
+        std::cout << ", " << pos_in_word_ << "\n";
     }
 };
 
@@ -23,7 +60,7 @@ public:
     bool predict (const std::string& string);
     bool predict (const char *string);
 private:
-    void scan(std::vector<std::set<Situation>> situation_lists, size_t cur_sit_idx, const char *string, size_t pos); 
-    void predict(std::vector<std::set<Situation>> situation_lists, size_t cur_sit_idx, const char *string, size_t pos); 
-    void complete(std::vector<std::set<Situation>> situation_lists, size_t cur_sit_idx, const char *string, size_t pos); 
+    void scan(std::vector<std::set<Situation>>& situation_lists, size_t cur_sit_idx, const char *string, size_t pos); 
+    void predict(std::vector<std::set<Situation>>& situation_lists, size_t cur_sit_idx, const char *string, size_t pos); 
+    void complete(std::vector<std::set<Situation>>& situation_lists, size_t cur_sit_idx, const char *string, size_t pos); 
 };
